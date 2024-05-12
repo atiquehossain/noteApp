8236eadfd005a2c0b30de7e9f8a6d46c413184ab
nsuat <nsuatique@gmail.com>
2024-05-12 22:23:49 +0600

    note repo

--- app/src/main/java/com/nexgenScript/noteapp/repository/NoteRepository.kt
+++ app/src/main/java/com/nexgenScript/noteapp/repository/NoteRepository.kt
@@ -1,9 +1,15 @@
 package com.nexgenScript.noteapp.repository
 
+import androidx.room.Query
 import com.nexgenScript.noteapp.database.NoteDatabase
 import com.nexgenScript.noteapp.model.Note
 
 class NoteRepository(private  val db: NoteDatabase) {
 
     suspend fun insertNote(note:Note) = db.getNoteDao().insertNote(note)
+    suspend fun  deleteNote(note: Note) = db.getNoteDao().deleteNote(note)
+    suspend fun  updateNote(note: Note) = db.getNoteDao().updateNote(note)
+     fun  getAllNotes(note: Note) = db.getNoteDao().getAllNotes()
+    fun searchNote(query: String?) = db.getNoteDao().searchNote(query)
+
 }
