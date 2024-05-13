a8cb564ca30e761a8aa5b7550114b69ac8a4afc8
nsuat <nsuatique@gmail.com>
2024-05-13 23:29:46 +0600

    update

---
+++ app/src/main/java/com/example/noteapp/repository/NoteRepository.kt
@@ -0,0 +1,14 @@
+package com.example.noteapp.repository
+
+import com.nexgenScript.noteapp.database.NoteDatabase
+import com.example.noteapp.model.Note
+
+class NoteRepository(private  val db: NoteDatabase) {
+
+    suspend fun insertNote(note: Note) = db.getNoteDao().insertNote(note)
+    suspend fun  deleteNote(note: Note) = db.getNoteDao().deleteNote(note)
+    suspend fun  updateNote(note: Note) = db.getNoteDao().updateNote(note)
+     fun  getAllNotes(note: Note) = db.getNoteDao().getAllNotes()
+    fun searchNote(query: String?) = db.getNoteDao().searchNote(query)
+
+}
