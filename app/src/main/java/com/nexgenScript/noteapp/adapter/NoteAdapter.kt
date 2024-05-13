23a17df6e1e831828ccab34067f1e252d871b88a
atique.hossain <atique.hossain@savethechildren.org>
2024-05-13 15:36:28 +0600

    Adapter emergency

--- app/src/main/java/com/nexgenScript/noteapp/adapter/NoteAdapter.kt
+++ app/src/main/java/com/nexgenScript/noteapp/adapter/NoteAdapter.kt
@@ -1,4 +1,37 @@
 package com.nexgenScript.noteapp.adapter
 
-class NoteAdapter {
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.nexgenScript.noteapp.databinding.NoteLayoutBinding
+import com.nexgenScript.noteapp.model.Note
+
+class NoteAdapter : RecyclerView.Adapter<NoteAdapter.NoteViewHolder>(){
+
+    class NoteViewHolder(val itemBinfing: NoteLayoutBinding): RecyclerView.ViewHolder(itemBinfing.root){
+
+        private val differCallback = object : DiffUtil.ItemCallback<Note>(){
+            override fun areItemsTheSame(oldItem: Note, newItem: Note): Boolean {
+                TODO("Not yet implemented")
+            }
+
+            override fun areContentsTheSame(oldItem: Note, newItem: Note): Boolean {
+                TODO("Not yet implemented")
+            }
+
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
 }
