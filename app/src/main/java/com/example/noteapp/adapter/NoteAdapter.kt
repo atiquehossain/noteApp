3abd50ec84436e983c3e56b4d860e59a4a7f8b78
nsuat <nsuatique@gmail.com>
2024-05-14 08:49:09 +0600

    NoteAdapter

--- app/src/main/java/com/example/noteapp/adapter/NoteAdapter.kt
+++ app/src/main/java/com/example/noteapp/adapter/NoteAdapter.kt
@@ -1,27 +1,49 @@
 package com.nexgenScript.noteapp.adapter
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.example.noteapp.databinding.NoteLayoutBinding
+import com.example.noteapp.model.Note
 
+class NoteAdapter : RecyclerView.Adapter<NoteAdapter.NotViewHolder>() {
 
-class NoteAdapter: RecyclerView.Adapter<NoteAdapter.NotViewHolder>() {
+    private val differCallback = object : DiffUtil.ItemCallback<Note>() {
+        override fun areItemsTheSame(oldItem: Note, newItem: Note): Boolean {
+            return oldItem.id == newItem.id
+        }
 
-    class  NotViewHolder(val  itemBinding: NoteLayoutBinding): RecyclerView.ViewHolder(itemBinding.root){
-
+        override fun areContentsTheSame(oldItem: Note, newItem: Note): Boolean {
+            return oldItem == newItem // This might not be ideal, consider content comparison
+        }
     }
 
-    
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteAdapter.NotViewHolder {
-        TODO("Not yet implemented")
+    private val differ = AsyncListDiffer(this, differCallback)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NotViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = NoteLayoutBinding.inflate(inflater, parent, false)
+        return NotViewHolder(binding)
     }
 
-    override fun onBindViewHolder(holder: NoteAdapter.NotViewHolder, position: Int) {
-        TODO("Not yet implemented")
+    override fun onBindViewHolder(holder: NotViewHolder, position: Int) {
+        val note = differ.currentList[position]
+        holder.bind(note)
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return differ.currentList.size
     }
 
+    class NotViewHolder(private val itemBinding: NoteLayoutBinding) :
+        RecyclerView.ViewHolder(itemBinding.root) {
+
+        fun bind(note: Note) {
+            itemBinding.noteTitle.text = note.noteTitle
+            itemBinding.noteDesc.text = note.noteDesc
+            // Bind other views if needed
+        }
+    }
 }
