4d82fcc5b865a44d0c9c7cce3e947f03c0e28dea
Atique <nsuatique@gmail.com>
2024-05-25 22:40:04 +0600

    Add fragment

--- app/src/main/java/com/example/noteapp/fragments/AddNoteFragment.kt
+++ app/src/main/java/com/example/noteapp/fragments/AddNoteFragment.kt
@@ -3,20 +3,40 @@ package com.example.noteapp.fragments
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.MenuProvider
 import com.example.noteapp.R
+import com.example.noteapp.databinding.FragmentAddNoteBinding
+import com.example.noteapp.viewmodel.NoteViewModel
+import com.nexgenScript.noteapp.adapter.NoteAdapter
 
 
-class AddNoteFragment : Fragment() {
+class AddNoteFragment : Fragment(R.layout.fragment_add_note) , MenuProvider {
 
+private var addNoteFragment: FragmentAddNoteBinding? =  null
+    private val binding get() = addNoteFragment !!
+    private lateinit var notesViewModel: NoteViewModel
+    private lateinit var noteAdapter: NoteAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_note, container, false)
+        addNoteFragment = FragmentAddNoteBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+        TODO("Not yet implemented")
     }
 
 
