8dc4772086855ac372c18f50990fdde01b2d2934
Atique <nsuatique@gmail.com>
2024-05-26 23:46:09 +0600

    Add fragment

--- app/src/main/java/com/example/noteapp/fragments/AddNoteFragment.kt
+++ app/src/main/java/com/example/noteapp/fragments/AddNoteFragment.kt
@@ -8,7 +8,10 @@ import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.MenuHost
 import androidx.core.view.MenuProvider
+import androidx.lifecycle.Lifecycle
+import com.example.noteapp.MainActivity
 import com.example.noteapp.R
 import com.example.noteapp.databinding.FragmentAddNoteBinding
 import com.example.noteapp.viewmodel.NoteViewModel
@@ -21,14 +24,24 @@ private var addNoteFragment: FragmentAddNoteBinding? =  null
     private val binding get() = addNoteFragment !!
     private lateinit var notesViewModel: NoteViewModel
     private lateinit var noteAdapter: NoteAdapter
+    private lateinit var addNoteView: View
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
         addNoteFragment = FragmentAddNoteBinding.inflate(inflater,container,false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val menuHost :MenuHost = requireActivity()
+        menuHost.addMenuProvider(this, viewLifecycleOwner, Lifecycle.State.RESUMED)
+        notesViewModel =
+            (activity as MainActivity).noteViewModel
     }
 
     override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
