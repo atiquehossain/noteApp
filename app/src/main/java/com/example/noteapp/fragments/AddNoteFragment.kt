03eba7a8082060dfce50dc150b9b4008072ecc5c
Atique <nsuatique@gmail.com>
2024-05-28 07:41:00 +0600

    Add fragment

--- app/src/main/java/com/example/noteapp/fragments/AddNoteFragment.kt
+++ app/src/main/java/com/example/noteapp/fragments/AddNoteFragment.kt
@@ -8,12 +8,14 @@ import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.core.view.MenuHost
 import androidx.core.view.MenuProvider
 import androidx.lifecycle.Lifecycle
 import com.example.noteapp.MainActivity
 import com.example.noteapp.R
 import com.example.noteapp.databinding.FragmentAddNoteBinding
+import com.example.noteapp.model.Note
 import com.example.noteapp.viewmodel.NoteViewModel
 import com.nexgenScript.noteapp.adapter.NoteAdapter
 
@@ -42,14 +44,42 @@ private var addNoteFragment: FragmentAddNoteBinding? =  null
         menuHost.addMenuProvider(this, viewLifecycleOwner, Lifecycle.State.RESUMED)
         notesViewModel =
             (activity as MainActivity).noteViewModel
+        addNoteView = view
+    }
+
+    private fun saveNote(view : View){
+        val noteTitle = binding.addNoteTitle.text.toString().trim()
+        val noteDesc = binding.addNoteDesc.text.toString().trim()
+
+        if (noteTitle.isNotEmpty()){
+            val  note = Note(0,noteTitle,noteDesc)
+            notesViewModel.addNote(note)
+            Toast.makeText(addNoteView.context,"bye",Toast.LENGTH_SHORT).show()
+        }
+        else{
+            Toast.makeText(addNoteView.context,"hi",Toast.LENGTH_LONG).show()
+
+        }
     }
 
     override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
-        TODO("Not yet implemented")
+        menu.clear()
+        menuInflater.inflate(R.menu.menu_add_note,menu)
     }
 
     override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
-        TODO("Not yet implemented")
+        return  when(menuItem.itemId){
+            R.id.saveMenu ->{
+                saveNote(addNoteView)
+                true
+            }
+            else -> false
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        addNoteFragment = null
     }
 
 
