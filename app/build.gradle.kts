60b7f93ca51092199f9a70f96f0c398925396166
Atique <nsuatique@gmail.com>
2024-05-30 06:44:09 +0600

    Add fragment

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    id("androidx.navigation.safeargs.kotlin") // Apply Safe Args plugin
 }
 
 android {
@@ -19,7 +20,6 @@ android {
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
@@ -39,12 +39,9 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
-
-
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
@@ -53,9 +50,8 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
     val nav_version = "2.7.7"
-
-
     // Kotlin
     implementation("androidx.navigation:navigation-fragment-ktx:$nav_version")
     implementation("androidx.navigation:navigation-ui-ktx:$nav_version")
@@ -65,13 +61,8 @@ dependencies {
     annotationProcessor("androidx.room:room-compiler:$room_version")
 
     val lifecycle_version = "2.7.0"
-
     // ViewModel
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycle_version")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycle_version")
-    implementation("androidx.lifecycle:lifecycle-compiler:2.7.0")
-
-
-
-
+    implementation("androidx.lifecycle:lifecycle-compiler:2.8.1")
 }
--- app/src/main/java/com/example/noteapp/fragments/EditNoteFragment.kt
+++ app/src/main/java/com/example/noteapp/fragments/EditNoteFragment.kt
@@ -1,37 +1,84 @@
 package com.example.noteapp.fragments
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.Menu
-import android.view.MenuInflater
-import android.view.MenuItem
-import android.view.View
-import android.view.ViewGroup
+import androidx.core.view.MenuHost
 import androidx.core.view.MenuProvider
+import androidx.lifecycle.Lifecycle
+import androidx.navigation.fragment.navArgs
+import com.example.noteapp.MainActivity
 import com.example.noteapp.R
 import com.example.noteapp.databinding.FragmentAddNoteBinding
+import com.example.noteapp.databinding.FragmentEditNoteBinding
+import com.example.noteapp.model.Note
+import com.example.noteapp.viewmodel.NoteViewModel
 
+class EditNoteFragment : Fragment(R.layout.fragment_edit_note), MenuProvider {
 
-class EditNoteFragment : Fragment(R.layout.fragment_edit_note) ,MenuProvider {
+    private var _binding: FragmentEditNoteBinding? = null
+    private val binding get() = _binding!!
 
+    private lateinit var noteViewModel: NoteViewModel
+    private lateinit var currentNote: Note
 
-    private  var editNoteBinding: FragmentAddNoteBinding? =null
-    private  val binding get() = editNoteBinding!!
-    
+    private val args: EditNoteFragmentArgs by navArgs()
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_edit_note, container, false)
+        _binding = FragmentEditNoteBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val menuHost: MenuHost = requireActivity()
+        menuHost.addMenuProvider(this, viewLifecycleOwner, Lifecycle.State.RESUMED)
+        noteViewModel = (activity as MainActivity).noteViewModel
+        currentNote = args.note // Access the passed note
+        populateNoteDetails()
+
+
+        binding.editNoteFab.setOnClickListener {
+            saveNote()
+        }
+    }
+
+    private fun populateNoteDetails() {
+        binding.editNoteTitle.setText(currentNote.noteTitle)
+        binding.editNoteDesc.setText(currentNote.noteDesc)
+    }
+
+    private fun saveNote() {
+        val title = binding.editNoteTitle.text.toString()
+        val description = binding.editNoteDesc.text.toString()
+        if (title.isNotEmpty() && description.isNotEmpty()) {
+            val updatedNote = currentNote.copy(noteTitle = title, noteDesc = description)
+            noteViewModel.updateNote(updatedNote)
+            // Navigate back or show a success message
+        } else {
+            // Show an error message if the title or description is empty
+        }
     }
 
     override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
-        TODO("Not yet implemented")
+        menuInflater.inflate(R.menu.menu_edit_note, menu)
     }
 
     override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
-        TODO("Not yet implemented")
+        return when (menuItem.itemId) {
+            R.id.saveMenu -> {
+                saveNote()
+                true
+            }
+            else -> false
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
